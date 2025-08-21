b18015a9b2efea5c6289399771c37426e77a92ef
thehxdev <hossein.khosravi.ce@gmail.com>
2025-08-21 14:17:16 +0330

    dont use callee saved registers in defer_call
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- x86_64_defer.s
+++ x86_64_defer.s
@@ -9,18 +9,18 @@ defer_call:
 	je		.do_call
 
     # save the function to call and args in temporary registers
-	movq	%rdi, %r12
-	movq	%rsi, %r14
+	movq	%rdi, %r10
+	movq	%rsi, %r11
 
     # assume sizeof(void*) == 8 bytes
-	movq	0x00(%r14), %rdi
-	movq	0x08(%r14), %rsi
-	movq	0x10(%r14), %rdx
-	movq	0x18(%r14), %rcx
-	movq	0x20(%r14), %r8
-	movq	0x28(%r14), %r9
+	movq	0x00(%r11), %rdi
+	movq	0x08(%r11), %rsi
+	movq	0x10(%r11), %rdx
+	movq	0x18(%r11), %rcx
+	movq	0x20(%r11), %r8
+	movq	0x28(%r11), %r9
 
 .do_call:
-	call	*%r12
+	call	*%r10
 	ret
 
